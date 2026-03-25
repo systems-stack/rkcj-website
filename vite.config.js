c5887a699b3618e04506ec20e03cf89e699588f5
systems-stack <systems@rohitkcjain.com>
2026-03-25 18:38:50 +0530

    Fix: Add trailing slash to base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rkcjwebsite/', // <-- Add this line exactly like this
+  base: "/Payroll_WebApp/", // This must match your repository name exactly
 })
