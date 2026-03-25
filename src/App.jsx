e86eecf799a27529dfc405725b9087fff1829159
systems-stack <systems@rohitkcjain.com>
2026-03-25 19:07:15 +0530

    Update config files for new repository name

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <BrowserRouter basename="/rkcjwebsite/">
+    <BrowserRouter basename="/rkcj-website">
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Payroll_WebApp/", // This must match your repository name exactly
+  base: "/rkcj-website/", // This must match your repository name exactly
 })
